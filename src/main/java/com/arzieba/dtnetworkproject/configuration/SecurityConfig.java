ca91ee8edc98648911cf4662dff95942763b9a96
arzieba1420 <arzieba@wszib.edu.pl>
2019-05-05 09:35:05 +0200

    Simple security

---
+++ src/main/java/com/arzieba/dtnetworkproject/configuration/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.arzieba.dtnetworkproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("DTN")
+                .password("DTN1")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("dtnetwork")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .hasAnyRole("USER","ADMIN")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+    }
+}
